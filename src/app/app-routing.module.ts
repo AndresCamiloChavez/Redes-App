314d7b92988957ac252c87b28d9915986bf9d213
Andres Chavez <andres.cz.chavez@gmail.com>
2022-02-28 21:02:30 -0500

    Manejo de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { TabsPageModule } from './tabs/tabs.module';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'tabs/tab1'
   }
 ];
 @NgModule({
