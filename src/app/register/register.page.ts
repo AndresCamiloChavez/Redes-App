a306d4fa6fca3b6dce361ebca45e585d52368d77
Andres Chavez <andres.cz.chavez@gmail.com>
2022-03-05 20:43:42 -0500

    Creación de usuarios

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  constructor(
+    private router: Router,
+    private auth: AngularFireAuth,
+    private toastCtrl: ToastController
+  ) {
+    this.buildForm();
+  }
 
   ngOnInit() {
+  }
+  buildForm(){
+    this.form = new FormGroup({
+      username: new FormControl('',[Validators.required]),
+      email: new FormControl('',[Validators.required, Validators.email]),
+      password: new FormControl('',[Validators.required]),
+    });
+  }
+  onRegister(){
+    this.auth.createUserWithEmailAndPassword(this.form.get('email').value, this.form.get('password').value).then(data =>{
+      this.presentToast();
+    });
+  }
+  onNavigateToLogin(){
+    this.router.navigate(['login']);
+  }
+  async presentToast(){
+    const toast = await this.toastCtrl.create({
+      header: 'Usuario creado',
+      message: 'Excelente se creo el usuario',
+    });
+    await toast.present();
   }
 
 }
